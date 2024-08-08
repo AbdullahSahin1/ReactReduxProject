b14e80c8e6c09082fccf696732bda9d90e3a37b0
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-08 16:48:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { useEffect } from 'react';
 import { calculateTotal } from './control/cardSlice';
 import { useSelector,useDispatch } from 'react-redux';
 import './App.css';
-
 function App() {
+  const {cartItems} = useSelector((store)=> store.cart)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(calculateTotal())
+  },[cartItems])
   return (
     <div className="App">
-          <Navbar></Navbar>
-                <CourseList></CourseList>
-
+      <Navbar></Navbar>
+      <CourseList></CourseList>
     </div>
   );
 }
