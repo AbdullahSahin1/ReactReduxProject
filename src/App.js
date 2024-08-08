2d88afaffccbf90d2ac2a3395f7ae1c806c74300
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-08 16:47:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar'
 import CourseList from './components/CourseList'
-
+import { useEffect } from 'react';
+import { calculateTotal } from './control/cardSlice';
+import { useSelector,useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
