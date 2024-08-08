9367367406383ca3373990aa437cb082c7b04098
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-08 13:18:08 +0300

    Update CourseItem.js

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -1,1 +1,23 @@
+import React from 'react'
+import { FaChevronUp } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa6";
 
+function CourseItem({id,title,price,img,quantity}) {
+  return (
+    <div>
+        <img src={img} alt=""/>
+        <div>
+            <h4>{title}</h4>
+            <h4>{price}</h4>
+            <div>
+                <button><FaChevronUp /></button>
+                <p>{quantity}</p>
+                <button><FaChevronDown/></button>
+            </div>
+        </div>
+    </div>
+    
+  )
+}
+
+export default CourseItem
