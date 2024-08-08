27c284fca43be7bf29c39ddb4a61c8f7bae143b8
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-08-08 13:16:59 +0300

    Update CourseItems.js

--- src/CourseItems.js
+++ src/CourseItems.js
@@ -1,30 +1,30 @@
 const CourseItems = [
     {
         id: 'adsadsadsadsada',
-        title: 'C Programlama Eğitimi',
+        title: 'Vue Eğitimi',
         price: '69',
-        img: 'https://www.canboz.com/Assets/img/canboz/c.png',
+        img: 'https://miro.medium.com/v2/1*OrjCKmou1jT4It5so5gvOA.jpeg',
         quantity: 1,
       },
       {
         id: 'sadsadsadsadsadsadsad',
-        title: 'C# Eğitimi',
+        title: 'React Eğitimi',
         price: '69',
-        img: 'https://www.canboz.com/Assets/img/canboz/csharp.png',
+        img: 'https://miro.medium.com/v2/resize:fit:1400/0*ECzYttBdIGxUbhOW.png',
         quantity: 1,
       },
       {
         id: 'sadsadsadsadsadsadsadsa',
-        title: 'Typescript Eğitimi',
+        title: 'Angular Eğitimi',
         price: '69',
-        img: 'https://www.canboz.com/Assets/img/canboz/typescript.jpg',
+        img: 'https://miro.medium.com/v2/resize:fit:783/1*Kj8Eq401fP2ecTY8r9B89Q.png',
         quantity: 1,
       },
       {
         id: 'adsadsadsadsadsadsadsaf',
-        title: 'React Eğitimi',
+        title: 'Node Eğitimi',
         price: '69 ',
-        img: 'https://www.canboz.com/Assets/img/canboz/react.png',
+        img: 'https://miro.medium.com/v2/resize:fit:800/1*v2vdfKqD4MtmTSgNP0o5cg.png',
         quantity: 1,
       },
     
